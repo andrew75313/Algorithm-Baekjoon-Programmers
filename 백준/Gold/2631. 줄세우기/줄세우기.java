600e298e55546fc7809a5e7e38c9092999102ddc
Hyeonjin Kim <161192870+andrew75313@users.noreply.github.com>
2025-04-12 13:11:50 +0900

    [Gold IV] Title: 줄세우기, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -15,30 +15,28 @@ public class Main {
             children[i] = num;
         }
 
-        // DP ( 값 : i 번째 까지 연속된 아이들의 최대의 수 )
-        int[] dp = new int[n];
+        // 최종 LIS 찾기
+        List<Integer> lisList =  new ArrayList<>();
 
-        // 초기값 등록
-        Arrays.fill(dp, 1);
+        for(int i = 0; i < n; i++) {
+            int index = Collections.binarySearch(lisList, children[i]) ;
 
-        // Children 순서대로 자기 앞이 자기보다 작으면 +1 추가
-        // dp 값은 j dp값 과 비교했을 때
-        for(int i = 1; i < n ; i++) {
-            for(int j = 0; j < i; j++) {
-                if(children[j] < children[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+            // LIS 에 없을 경우 index 재지정
+            if (index < 0) index = Math.abs(index) - 1;
+
+            // index LIS 보다 작을 경우 -> 해당 위치 update 세팅
+            if(index < lisList.size()) {
+                lisList.set(index, children[i]);
+            }
+
+            // 크기랑 같거나 클 경우 뒤에 붙이기 -> 뒤에 세팅
+            if(index >= lisList.size()) {
+                lisList.add(children[i]);
             }
         }
 
-        // dp 값중 최대 값 = 가장 긴 열 갯수 를 전체 수에서 뻄
-        int result = 0;
-        int maxLength = 0;
-        for(int childrenLength : dp) {
-            maxLength = Math.max(maxLength, childrenLength);
-        }
-        
-        result = n - maxLength;
+        int result = n - lisList.size();
+
         System.out.println(result);
     }
 }
